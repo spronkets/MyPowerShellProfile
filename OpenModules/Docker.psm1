126e83a8103a480fd1ebe0afc069f0d379767759
Kody Crossman <k.crossman@ymail.com>
2025-02-21 20:21:02 -0700

    feat: updated Docker database installs

--- OpenModules/Docker.psm1
+++ OpenModules/Docker.psm1
@@ -1,17 +1,26 @@
+$rootUser = 'dev'
+$rootPassword = 'Password!2'
+
 function Install-DockerLocalnet {
   & docker network create --subnet=172.18.0.0/16 localnet
 }
 
 function Install-DockerSqlServerDb {
-  & docker run --name sqlserver --restart always -it -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Password!2" --net localnet --ip 172.18.0.22 -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+  # default user: sa, cannot be overriden
+  & docker run --name sqlserver --restart unless-stopped -it -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$rootPassword" -e "MSSQL_PID=Express" --net localnet --ip 172.18.0.22 -p 1433:1433 -d mcr.microsoft.com/mssql/server:latest
 }
 
 function Install-DockerPostgresDb {
-  # note: doesn't currently use localnet
-  & docker run --name postgres --restart always -it -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -p 5432:5432 -d postgres
+  # default user: postgres, overriden by $rootUser
+  & docker run --name postgres --restart unless-stopped -it -e POSTGRES_USER=$rootUser -e POSTGRES_PASSWORD=$rootPassword --net localnet --ip 172.18.0.24 -p 5432:5432 -d postgres:latest
 }
 
-function Invoke-DockerCompose {
-  & docker build
-  & docker-compose up -d
+function Install-DockerMysqlDb {
+  # default user: root, cannot be overriden
+  & docker run --name mysql --restart unless-stopped -it -e MYSQL_ROOT_PASSWORD=$rootPassword --net localnet --ip 172.18.0.23 -p 3306:3306 -d mysql:latest
+}
+
+function Install-DockerMongoDb {
+  # no default user, set by $rootUser
+  & docker run --name mongo --restart unless-stopped -it -e MONGO_INITDB_ROOT_USERNAME=$rootUser -e MONGO_INITDB_ROOT_PASSWORD=$rootPassword --net localnet --ip 172.18.0.25 -p 27017:27017 -d mongo:latest
 }
